bd57867f738a82466268c3f2ae23181b52779af6
Stewie Po <devolper7@gmail.com>
2019-04-30 22:48:42 +0300

    Init commit

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Card</h3>
+      </div>
+    );
+  }
+}
+
+export default Card;
